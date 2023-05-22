b3065b172dafa642474bf3d5f86839024b2ed7c5
Giacomo Dante Carbonara <gcarbonara3@gmail.com>
2023-05-22 14:52:03 +0200

    railed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,12 +4,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:input]
-    if @question == 'I am going to work'
-      @answer = 'Great!'
-    elsif @question == '?'
-      @answer = 'Silly question, get dressed and go to work!'
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
+    @answer = if @question == 'I am going to work'
+                'Great!'
+              elsif @question.include? '?'
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
